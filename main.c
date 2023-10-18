a2453e1b8e8b24740ebbd338798e86d4f14f7ae0
loafbud <143001064+loafbud@users.noreply.github.com>
2023-10-18 18:19:59 -0500

    Checkpiont

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+ * Lab06.c
+ *
+ * Created: 10/18/2023 6:14:35 PM
+ * Author : Hayden Smith
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) {
+		// this is my C code
+    }
+	return 0;
+}
+
